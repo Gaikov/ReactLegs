7a7c5397544ebf44da1dc42cc8c3a5deb270dcf0
Roman Gaikov <gaikov@inbox.ru>
2018-11-18 11:10:14 +0300

    https://github.com/Gaikov/ReactLegs/issues/1 - removed unnecessary code

--- tests/AppConfig.ts
+++ tests/AppConfig.ts
@@ -12,7 +12,6 @@ import {CreateListCommand} from "./commands/CreateListCommand";
 import {ItemRenderer} from "./views/ItemRenderer";
 import {ItemRendererMediator} from "./views/ItemRendererMediator";
 import {CoreSignal} from "./signals/CoreSignal";
-import {ItemRendererMediator2} from "./views/ItemRendererMediator2";
 import {IConfig} from "../src/mvc/context/IConfig";
 import {IMapping} from "../src/mvc/context/IMapping";
 
@@ -25,7 +24,6 @@ export class AppConfig implements IConfig {
 
         context.mapMediator(ItemsListView, ItemsListMediator);
         context.mapMediator(ItemRenderer, ItemRendererMediator);
-        context.mapMediator(ItemRenderer, ItemRendererMediator2);
 
         context.mapCommand(CoreSignal.CREATE_LIST, CreateListCommand);
     }
